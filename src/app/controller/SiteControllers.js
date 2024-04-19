9307f10ba8cc852adfe789b030d5f1c441a8f762
M-KC <macbookpro@MacBookPros-MacBook-Pro.local>
2024-04-19 23:35:29 +0700

    update db

--- src/app/controller/SiteControllers.js
+++ src/app/controller/SiteControllers.js
@@ -1,7 +1,22 @@
+const Course = require('../models/Course');
 class SiteController {
     //[Get] /home
-    index(req, res) {
-        res.render('home');
+    index(req, res, next) {
+
+
+        Course.find().exec()
+            .then(courses => {
+                // Xử lý kết quả
+                courses = courses.map(course => course.toObject())
+                res.render('home', {courses});
+            })
+            .catch(err => {
+                // Xử lý lỗi
+                next(err);
+            });
+
+
+        // res.render('home');
     }
 
     // [Get] /search
--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes');
+const db = require('./config/db');
+
+
+// connect to db
+db.connect();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -27,11 +32,12 @@ app.engine(
   }),
 );
 app.set('view engine', 'hbs');
-      app.set('views', path.join(__dirname, 'resources/views'));
+app.set('views', path.join(__dirname, 'resources', 'views'));
 
 // Routes init
 route(app);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, 
+  () => {
+  console.log(`App listening on port ${port}`);
 });
